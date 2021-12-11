e19f9c87300ba9817faa2f1ceb0fddd76fa7b992
EC2 Default User <ec2-user@ip-172-31-16-246.ec2.internal>
2021-12-11 09:51:03 +0000

    error delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
 
   def create
-    @bbok = Book.new(book_params)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to books_path
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, )
+    params.require(:book).permit(:title, :body,)
   end
 
 end
